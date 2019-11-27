3fbe0539e79935b15cc63fd1b40a05909659f44c
IagorSs <54728248+IagorSs@users.noreply.github.com>
2019-11-27 00:23:16 -0200

    Esqueci de uma parada, juro q é o ultimo commit

--- Projeto/src/Modelo/JogoCasino.java
+++ Projeto/src/Modelo/JogoCasino.java
@@ -42,6 +42,7 @@ public abstract class JogoCasino {
     
     //Esse método deve retornar as instruções q deverão aparecer quando clicado em instruções ou help na tela do jogo
     public abstract String instrucoesDoJogo();
+    //Esse método ainda está em discussão sobre ser lucro do dia, ou da partida ou seilá, mas vai ser or eferencial pro casino ter controle de quanto está lucrando
     public abstract Integer lucroDoDia();
     
     //tira isso daqui antes de entregar o trabalho e bota um valor lá q tá bom
